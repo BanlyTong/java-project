393fb9082db0ca0e9a94941bb6b67b02e8666ac2
Monysathya <monysathya.suos@gmail.com>
2018-04-22 10:39:52 +0700

    Add Database class

---
+++ Welcome_Screen_Copy00-02/src/Database.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sathya
+ */
+public class Database {
+    public static String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String url = "jdbc:sqlserver://localhost:1433;databasename=HealthCareService;user=sa;password=sathya123;";
+}
